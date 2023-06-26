548bb92d341e30b4adb02516de1e92b1dabdb24d
Dean Whitbread <dean_whitbread@hotmail.com>
2023-06-26 15:55:35 +0100

    Add dataset folder and root folder with initial files.
    - Add init.py files.
    - Add main.py file inside root folder.

---
+++ root/main.py
@@ -0,0 +1,9 @@
+'''
+    The main file that executes the experiments. 
+    
+    @author Dean Whitbread
+    @version 26-06-2023
+'''
+
+# TODO: Implement experiments for XAI 
+#       tools LIME, SHAP, and Grad-CAM. 
