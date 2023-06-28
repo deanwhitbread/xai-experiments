c4f3c2ddf963b269d4bcd157c12396da63dc6352
Dean Whitbread <dean_whitbread@hotmail.com>
2023-06-28 08:13:41 +0100

    Add wrapper.py helper file and model to be used, and update main.py.

--- src/main.py
+++ src/main.py
@@ -6,25 +6,60 @@
     Version:
         26-06-2023
 '''
+import os
+import pandas as pd
+import wrapper
+from sklearn.model_selection import train_test_split
 
+# Constants
 XAI_CHOICES = ['LIME', 'SHAP', 'Grad-CAM']
+DATASET_PATH = '../dataset/MICCAI_BraTS_2018_Data_Training'
+MODEL_PATH = '../models/cnn-parameters-improvement-23-0.91.model'
 
 print('Welcome!')
 
-''' Load the dataset '''
+# Load the dataset
 print('Loading dataset...')
+#print(os.listdir(f'{DATASET_PATH}/HGG/Brats18_2013_2_1'))
+brats_2018 = pd.read_csv(f'{DATASET_PATH}/survival_data.csv', sep=",")
 
-''' Train the model '''
-print('Preparing model...')
 
-''' Make a prediction '''
+#url: https://github.com/MohamedAliHabib/Brain-Tumor-Detection/blob/master/Brain%20Tumor%20Detection.ipynb
+
+from tensorflow.keras.models import load_model
+
+img = wrapper.run(
+#pred = wrapper.get_prediction(
+        f'{DATASET_PATH}/HGG/Brats18_2013_10_1/jpg/output-slice070.jpg',    # test a single image 
+        MODEL_PATH 
+        #load_model(MODEL_PATH), 240, 240
+    )
+
+#print(pred)
+
+'''
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import cv2
+
+img_1 = wrapper.crop(cv2.imread(f'{DATASET_PATH}/HGG/Brats18_2013_10_1/jpg/output-slice070.jpg'))
+
+plt.figure()
+plt.imshow(img_1)
+plt.show()
+'''
+
+# Train the model
+print('Training model...')
+
+# Make a prediction
 print('Creating predictions...')
 
-''' Interpret the prediction '''
+# Interpret the prediction
 while True:
     print('\nHow do you want to interpret the predictions?')
     
-    # Prepare available options
+    ''' Prepare available options '''
     avail_opts = ""
     for choice in XAI_CHOICES:
         if choice != XAI_CHOICES[len(XAI_CHOICES)-1]:
@@ -34,7 +69,7 @@ while True:
 
     opt = input(f'Choices: {avail_opts}, quit: ').lower().strip()
 
-    # Show XAI interpretation or quit
+    ''' Show XAI interpretation or quit '''
     if opt == 'quit':
         print('Goodbye')
         break;
---
+++ src/wrapper.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+import numpy as np
+import cv2
+import imutils
+
+
+def crop(image):
+    # Convert the image to grayscale, and blur it slightly
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+
+    # Threshold the image, then perform a series of erosions +
+    # dilations to remove any small regions of noise
+    thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY)[1]
+    thresh = cv2.erode(thresh, None, iterations=2)
+    thresh = cv2.dilate(thresh, None, iterations=2)
+
+    # Find contours in thresholded image, then grab the largest one
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    c = max(cnts, key=cv2.contourArea)
+
+    # Find the extreme points
+    extLeft = tuple(c[c[:, :, 0].argmin()][0])
+    extRight = tuple(c[c[:, :, 0].argmax()][0])
+    extTop = tuple(c[c[:, :, 1].argmin()][0])
+    extBot = tuple(c[c[:, :, 1].argmax()][0])
+
+    # crop new image out of the original image using the four extreme points (left, right, top, bottom)
+    return image[extTop[1] : extBot[1], extLeft[0] : extRight[0]]
+
+
+def prepare_image(path, x, y):
+    img = cv2.imread(path)
+    img = crop(img)
+    img = cv2.resize(img, dsize=(x, y), interpolation=cv2.INTER_CUBIC)
+    img = img / 255.0
+    return np.expand_dims(img, axis=0)
+
+
+def get_prediction(path, model, x=240, y=240):
+    img = prepare_image(path, x, y)
+    return model.predict(img, verbose=0)
+
+
+def run(path, model_path):
+    model = load_model(model_path)
+    predictions = get_prediction(path, model)
+    return np.where(predictions[0] > 0.5, 1, 0)[0]
