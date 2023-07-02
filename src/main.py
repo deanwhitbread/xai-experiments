5eb13030f4e41b9f6188fdc53110dc91e05e363a
Dean Whitbread <dean_whitbread@hotmail.com>
2023-07-02 20:34:33 +0100

    Initial commit.

--- src/main.py
+++ src/main.py
@@ -11,6 +11,7 @@ from tensorflow.keras.models import load_model
 from helpers import get_next_image_path, get_shortcut_key_str, get_shortcut_key, get_choices
 from xai.grad_cam_xai import GradCam
 from xai.lime_xai import Lime
+from xai.shap_xai import Shap
 
 # Constants
 XAI_CHOICES = [
---
+++ src/xai/shap_xai.py
@@ -0,0 +1,13 @@
+'''
+    shap_xai.py contains the class to 
+    interpret predictions using SHAP XAI. 
+
+    Author:
+        Dean Whitbread
+    Version: 
+        02-07-2023
+'''
+
+class Shap:
+    # TODO: Implement SHAP XAI to interpret predictions. 
+    pass
