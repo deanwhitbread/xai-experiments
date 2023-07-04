45d6bc8f1f04beb01947122d4e39d945e3cfe4c6
Dean Whitbread <dean_whitbread@hotmail.com>
2023-07-04 19:25:47 +0100

    Implement SHAP interpreter.
    - Implement SHAP interpreter in shap_xai.py
    - Create get_dataset_image method and other methods in helpers.py
    - Remove reading dataset from main.py

--- src/main.py
+++ src/main.py
@@ -8,10 +8,12 @@
 '''
 import pandas as pd
 from tensorflow.keras.models import load_model
-from helpers import get_next_image_path, get_shortcut_key_str, get_shortcut_key, get_choices
+from helpers import get_next_image_path, get_shortcut_key_str, get_shortcut_key, get_choices, get_dataset_images
 from xai.grad_cam_xai import GradCam
 from xai.lime_xai import Lime
 from xai.shap_xai import Shap
+from wrapper import run as predict
+from os import listdir
 
 # Constants
 XAI_CHOICES = [
@@ -22,15 +24,18 @@ XAI_CHOICES = [
 DATASET_PATH = '../dataset/MICCAI_BraTS_2018_Data_Training'
 MODEL_PATH = '../models/cnn-parameters-improvement-23-0.91.model'
 
-print('Welcome!\nLoading dataset and model...')
-brats_2018 = pd.read_csv(f'{DATASET_PATH}/survival_data.csv', sep=",")
+print('Welcome!\nLoading model...')
 model = load_model(MODEL_PATH)
 
-print('Choosing image...')
+print('Choosing first image...')
 image_path = get_next_image_path(DATASET_PATH)
 
+print('Generating a list of dataset images...')
+images = get_dataset_images(DATASET_PATH)
+
 while True:
-    print('\nHow do you want to interpret the prediction?')
+    print(f'\nModel Prediction: {predict(image_path, model)}')
+    print('How do you want to interpret the prediction?')
     
     opt = input(f'Choices: {get_choices(XAI_CHOICES)}:').lower().strip()
 
@@ -40,8 +45,7 @@ while True:
     elif opt == XAI_CHOICES[0].lower() or opt == get_shortcut_key(XAI_CHOICES[0]):
         xai = Lime(image_path, model)
     elif opt == XAI_CHOICES[1].lower() or opt == get_shortcut_key(XAI_CHOICES[1]):
-        # SHAP
-        continue;
+        xai= Shap(image_path, model, images)
     elif opt == XAI_CHOICES[2].lower() or opt == get_shortcut_key(XAI_CHOICES[2]):
         xai = GradCam(image_path, model)
     else:
--- src/xai/shap_xai.py
+++ src/xai/shap_xai.py
@@ -5,9 +5,43 @@
     Author:
         Dean Whitbread
     Version: 
-        02-07-2023
+        04-07-2023
 '''
+# ignore warning messages
+from warnings import filterwarnings
+filterwarnings("ignore", message=".*The 'nopython' keyword.*")
+
+import shap
+import wrapper
+import cv2
 
 class Shap:
-    # TODO: Implement SHAP XAI to interpret predictions. 
-    pass
+    def __init__(self, impath, model, images):
+        '''Construct the Shap object.
+            
+        Arguments:
+            impath: The path to the target image.
+            model: The classification model used to classify the image. 
+            images: A numpy matrix of all the images in the dataset. 
+        '''
+        self.target_image = wrapper.crop(cv2.imread(impath))
+        self.target_image = cv2.resize(self.target_image, dsize=(240, 240), interpolation=cv2.INTER_CUBIC)
+        self.images = images
+        self.images.append(wrapper.prepare_image(impath))
+        self.exp = self.get_explaination(model)
+
+    def get_explaination(self, model):
+        '''Return an explaination of the prediction using SHAP. 
+
+        Arguments:
+            model: The classification model used to classify the image. 
+            images: A numpy matrix of all the images in the dataset. 
+        '''
+        masker = shap.maskers.Image('blur(240,240)', self.target_image.shape)
+        explainer = shap.Explainer(model, masker)
+
+        return explainer(self.images[-1], max_evals=5000, batch_size=100, outputs=shap.Explanation.argsort.flip[:4])
+
+    def show(self):
+        '''Display the target image and the XAI interpretation. '''
+        shap.image_plot(self.exp)
