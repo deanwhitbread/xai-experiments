2a279b9c2698a9dea2c1e060427717e891cc2444
Dean Whitbread <dean_whitbread@hotmail.com>
2023-07-06 19:58:26 +0100

    Remove unused methods from helpers.py

--- src/misc/helpers.py
+++ src/misc/helpers.py
@@ -14,43 +14,6 @@ import misc.wrapper as wrapper
 
 rand.seed(3)
 
-#'''
-#def __get_next_directory_name(path):
-#    ''' Return the name of a randomly selected directory. 
-#    
-#    Arguments:
-#        path: The path of the current directory where child
-#               directory folders are located. 
-#    '''
-#    dir_list = next(os.walk(path))[1]
-#    index = rand.randint(0, len(dir_list)-1)
-#    return dir_list[index]
-#
-#def __get_image_name(path):
-#    ''' Return the name of a randomly selected image.
-#
-#    Arguments:
-#        path: The path of the current directory where the 
-#               image file are located. 
-#    '''
-#    image_list = os.listdir(path)
-#    return image_list[rand.randint(0, len(image_list)-1)]
-#
-#def get_next_image_path(path):
-#    ''' Return the path where the randomly selected image
-#        is located.
-#
-#    Arguments:
-#        path: The initial path to start the selection.
-#        
-#    '''
-#    while len(next(os.walk(path))[1]):
-#        next_dir = __get_next_directory_name(path)
-#        path = f'{path}/{next_dir}'
-#
-#    return f'{path}/{__get_image_name(path)}'
-#
-
 def get_shortcut_key_str(word, key):
     '''Return a string highlighting the shortcut key.
 
