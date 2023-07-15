af2512378e43f7ec2a343db751dc6f5dfd61bfcb
Dean Whitbread <dean_whitbread@hotmail.com>
2023-07-15 17:58:16 +0100

    Change method names and local attributes in tumor_detector.py.

--- src/analyser/tumor_detector.py
+++ src/analyser/tumor_detector.py
@@ -1,6 +1,5 @@
 '''
-    The TumorDetector class contains methods to detect tumors in 
-    MRI images.
+    This file contains methods to detect brain tumors in MRI scans.
 '''
 __author__ = 'Dean Whitbread'
 __version__ = '15-07-2023'
@@ -8,18 +7,16 @@ __version__ = '15-07-2023'
 import cv2
 import numpy as np
 
-#class TumorDetector:
-#    def __init__(self):
-#        pass
+RGB_THRESHOLD = (160,160,160)
 
-def detect_and_highlight_tumor(image):
-    # Convert the image to grayscale
-    grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def __get_detected_circles(blurred_image):
+    '''Return a list of detected circles.
 
-    # Apply Gaussian blur to reduce noise and improve circle detection
-    blurred_image = cv2.GaussianBlur(grayscale_image, (9, 9), 2)
+    If no circles are detected, None is returned.
 
-    # Detect circles using Hough Circle Transform
+    Parameter:
+    blurred_image: The target image with a gaussian blur applied. 
+    '''
     circles = cv2.HoughCircles(
         blurred_image,
         cv2.HOUGH_GRADIENT,
@@ -31,21 +28,43 @@ def detect_and_highlight_tumor(image):
         maxRadius=60,
     )
 
+    return circles
+
+def detect_and_highlight_tumor(image):
+    '''Return the image with any brain tumors highlighed.
+    
+    If no tumors are detected, the original image is returned.
+    
+    Parameters:
+    image: The original image to detect brain tumors from.
+    '''
+    grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Apply Gaussian blur to reduce noise and improve circle detection
+    blurred_image = cv2.GaussianBlur(grayscale_image, (9, 9), 2)
+
+    circles = __get_detected_circles(blurred_image)
+    
     if circles is not None:
         circles = np.round(circles[0, :]).astype("int")
         for (x, y, r) in circles:
-            centre_pixel_colour = check_pixel_color(image, x, y)
-            threshold = 160
-            if centre_pixel_colour >= (threshold, threshold, threshold):
+            if __check_pixel_colour(image,x,y) >= RGB_THRESHOLD:
                 # Draw the circle on the original image
-                cv2.circle(image, (x, y), r, (0, 255, 0), 4)
+                cv2.circle(image, (x, y), r, (255, 0, 0), 4)
 
     return image
 
-def check_pixel_color(image, x, y):
-    # Get the color values at the specified pixel (x, y)
+def __check_pixel_colour(image, x, y):
+    '''Return the pixel colour in the format: (R, G, B). 
+
+    Parameters:
+    image: The image being checked.
+    x: The x-coordinate.
+    y: The y-coordinate.
+    '''
     b, g, r = image[y, x]
 
     # Print the RGB values
-    print(f"Pixel at ({x}, {y}) has RGB color: ({r}, {g}, {b})")
+    #print(f"Pixel at ({x}, {y}) has RGB color: ({r}, {g}, {b})")
+
     return (r, g, b)
