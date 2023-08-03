97fa331a7e708cb0d37957741f2746d160b527d8
Dean Whitbread <dean_whitbread@hotmail.com>
2023-08-03 13:24:27 +0100

    Create initial script to select jpg images.

---
+++ scripts/select_jpg_images.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+# Select jpg images to use with the experiement. 
+# 
+# This script randomly selects 10 jpg images from each subfolder in the
+# dataset. The script uses a number seeder to ensure the same images 
+# are selected everytime the script is executed.
+#
+# Script is executed using the command: bash select_jpg_images.sh
+
+RANDOM=10
